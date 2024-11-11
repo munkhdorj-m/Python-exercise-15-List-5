8ba3b567cd88e438f126742fef3821e3aff03631
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-11 09:48:33 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,16 +1,16 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def sum_of_digits(numbers):
+def index_of_highest_number(numbers):
     # Write your code here
     pass
 
 # Exercise 2
-def largest_negative_and_smallest_positive(numbers):
+def find_all_indices(numbers, target):
     # Write your code here
     pass
 
 # Exercise 3
-def odd_first_last_digits(numbers):
+def swap_even_odd_indices(numbers):
     # Write your code here
     pass
